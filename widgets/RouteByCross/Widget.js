e7f7377066163e02cf8b463bab7b3ffd6f6ebdc9
晓青 史 <herrshi@hotmail.com>
2017-12-20 17:50:55 +0800

    22

--- widgets/RouteByCross/Widget.js
+++ widgets/RouteByCross/Widget.js
@@ -6,103 +6,159 @@ define([
   "dojo/_base/declare",
   "dojo/_base/lang",
   "dojo/_base/array",
-  "dojo/dom-style",
   "dojo/keys",
   "dojo/query",
   "dojo/dom-attr",
-  "dijit/TooltipDialog",
-  "dijit/popup",
+  "dojo/request/xhr",
   "jimu/BaseWidget",
+  "esri/Color",
+  "esri/graphic",
   "esri/layers/FeatureLayer",
-  "esri/renderers/jsonUtils"
+  "esri/layers/GraphicsLayer",
+  "esri/symbols/PictureMarkerSymbol",
+  "esri/symbols/SimpleLineSymbol",
+  "esri/renderers/SimpleRenderer"
 ], function (
   declare,
   lang,
   array,
-  domStyle,
   keys,
   query,
   domAttr,
-  TooltipDialog,
-  dijitPopup,
+  xhr,
   BaseWidget,
+  Color,
+  Graphic,
   FeatureLayer,
-  rendererJsonUtils
+  GraphicsLayer,
+  PictureMarkerSymbol,
+  SimpleLineSymbol,
+  SimpleRenderer
 ) {
   return declare([BaseWidget], {
     name: "RouteByCross",
     baseClass: "jimu-widget-RouteByCross",
 
+    //初始显示所有路口, 保存路口的graphic
     crossLayer: null,
+    //初始显示所有的道路, 保存道路的graphic
+    roadLayer: null,
+    //显示组成路径的路口和发布段
+    routeLayer: null,
 
-    tooltipDialog: null,
+    //未选\待选的路口symbol
+    unSelectedCrossSymbol: null,
+    //未选\待选的道路symbol
+    unSelectedRoadSymbol: null,
+    //选中的路口symbol
+    selectedCrossSymbol: null,
+    //选中的道路symbol
+    selectedRoadSymbol: null,
 
-    clickedGraphic: null,
-    selectedCrossGraphic: [],
+    //路口与道路关系表
+    crossRoadTable: [],
+
+    selectedCrossGraphics: [],
 
     postCreate: function () {
       this.inherited(arguments);
 
-      this.tooltipDialog = new TooltipDialog({
-        id: "tooltipDialog",
-        style: "position: absolute; width: 250px; font: normal normal normal 10pt Helvetica;z-index:100"
-      });
+      this.unSelectedCrossSymbol = new PictureMarkerSymbol(window.path + "images/BlueSphere.png", 24, 24);
+      this.selectedCrossSymbol = new PictureMarkerSymbol(window.path + "images/RedSphere.png", 48, 48);
 
+      this.unSelectedRoadSymbol = new SimpleLineSymbol( SimpleLineSymbol.STYLE_DASH, new Color([0, 0, 255]), 2);
+      this.selectedRoadSymbol = new SimpleLineSymbol( SimpleLineSymbol.STYLE_SOLID, new Color([255, 0, 0]), 4);
+
+      this.routeLayer = new GraphicsLayer();
+      this.map.addLayer(this.routeLayer);
+
+      this._readCrossRoadTable();
       this._showAllCross(this.config);
     },
 
     _showAllCross: function (config) {
       var url = config.crossLayer.replace(/{gisServer}/i, this.appConfig.gisServer);
-      var renderer = rendererJsonUtils.fromJson(config.crossRenderer);
       this.crossLayer = new FeatureLayer(url, {
         outFields: ["*"]
       });
-      this.crossLayer.renderer = renderer;
+      this.crossLayer.renderer = new SimpleRenderer(this.unSelectedCrossSymbol);
       this.crossLayer.on("mouse-over", lang.hitch(this, this._crossLayer_onMouseOver));
-      this.crossLayer.on("mouse-out", lang.hitch(this, this._crossLayer_onMouseOut));
-      this.crossLayer.on("click", lang.hitch(this, this._crossLayer_onClick));
-
       this.map.addLayer(this.crossLayer);
     },
 
+    _readCrossRoadTable: function () {
+      xhr(window.path + "configs/RouteByCross/CrossRoad.csv", {
+        handleAs: "text"
+      }).then(lang.hitch(this, function (data) {
+        var allRows = data.split(/\r?\n|\r/);
+        array.forEach(allRows, function (row) {
+          var rowCells = row.split(",");
+          if (rowCells.length >= 3) {
+            this.crossRoadTable.push({
+              startCrossId: rowCells[0],
+              endCrossId: rowCells[1],
+              roadIds: rowCells[2].split("/")
+            });
+          }
+        }, this);
+      }));
+    },
+
     _crossLayer_onMouseOver: function (event) {
-      //显示路口名称
-      var content = "<b>" + event.graphic.attributes[this.config.crossNameField] + "</b>";
-      this.tooltipDialog.setContent(content);
-      domStyle.set(this.tooltipDialog.domNode, "opacity", 0.85);
-      dijitPopup.open({
-        popup: this.tooltipDialog,
-        around: event.graphic.getNode()
-      });
-
+      this._showStartRouteConfirmPopup(event.graphic);
     },
 
-    _crossLayer_onMouseOut: function (event) {
-      dijitPopup.close(this.tooltipDialog);
-    },
-
-    _crossLayer_onClick: function (event) {
-      //路口名称
-      dijitPopup.close(this.tooltipDialog);
-
-      this.clickedGraphic = event.graphic,
-      this._showStartRouteConfirmPopup(event.graphic.geometry);
+    _getGraphicFromFeatureLayer: function (id, featureLayer) {
+      var filter = array.filter(featureLayer.graphics, function (graphic) {
+        return graphic.attributes[this.config.crossIdField] === id;
+      }, this);
+      if (filter.length > 0) {
+        return filter[0];
+      }
+      else {
+        return null;
+      }
     },
 
     /**显示是否开始选择路径的确认框*/
-    _showStartRouteConfirmPopup: function (mapPoint) {
+    _showStartRouteConfirmPopup: function (graphic) {
       var content = "<b>是否开始选择路径?</b><hr>" +
-        "<button type='button' class='btn btn-primary btn-xs' id='btnStartSelectRoute' >确定</button>  " +
+        "<button type='button' class='btn btn-primary btn-xs' id='btnStartSelectRoute' data-crossId='" + graphic.attributes[this.config.crossIdField] + "' >确定</button>  " +
         "<button type='button' class='btn btn-primary btn-xs' id='btnCancelSelectRoute' >取消</button>";
       this.map.infoWindow.setContent(content);
-      this.map.infoWindow.show(mapPoint);
+      this.map.infoWindow.setTitle(graphic.attributes[this.config.crossNameField]);
+      this.map.infoWindow.show(graphic.geometry);
 
       query("button#btnStartSelectRoute").on("click", lang.hitch(this, this._onBtnStartSelectRouteClick));
       query("button#btnCancelSelectRoute").on("click", lang.hitch(this, this._onBtnCancelSelectRouteClick));
     },
 
-    _onBtnStartSelectRouteClick: function (event) {
+    _selectCross: function (graphic) {
+      //显示选中路口
+      var selectedGraphic = new Graphic(graphic.geometry, this.selectedCrossSymbol);
+      this.routeLayer.add(selectedGraphic);
+      this.map.centerAt(graphic.geometry);
 
+      //显示下游路口
+      array.forEach(this.crossRoadTable, function (crossRoadObj) {
+        if (graphic.attributes[this.config.crossIdField] === crossRoadObj.startCrossId) {
+          var endGraphic = this._getGraphicFromFeatureLayer(crossRoadObj.endCrossId, this.crossLayer);
+          //待选的路口
+          var crossGraphic = new Graphic(endGraphic.geometry, this.unSelectedCrossSymbol);
+          this.routeLayer.add(crossGraphic);
+        }
+      }, this);
+
+    },
+
+    _onBtnStartSelectRouteClick: function (event) {
+      this.crossLayer.hide();
+      this.map.infoWindow.hide();
+
+      var crossId = domAttr.get(event.target, "data-crossId");
+      console.log(crossId);
+      var graphic = this._getGraphicFromFeatureLayer(crossId, this.crossLayer);
+      this._selectCross(graphic);
     },
 
     _onBtnCancelSelectRouteClick: function (event) {
