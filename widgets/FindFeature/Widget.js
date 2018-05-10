cc76f3e484a0db0785a5284c490b7077557d0fb0
晓青 史 <herrshi@hotmail.com>
2018-05-10 15:27:05 +0800

    虹桥项目的停车场改为feature图层的图标 + dynamic图层的label

--- widgets/FindFeature/Widget.js
+++ widgets/FindFeature/Widget.js
@@ -5,6 +5,7 @@ define([
   "dojo/topic",
   "dojo/promise/all",
   "jimu/BaseWidget",
+  "jimu/utils",
   "jimu/dijit/LoadingIndicator",
   "esri/layers/GraphicsLayer",
   "esri/layers/ArcGISDynamicMapServiceLayer",
@@ -15,23 +16,24 @@ define([
   "esri/tasks/QueryTask",
   "esri/tasks/query",
   "esri/Color"
-], function (
-    declare,
-    lang,
-    array,
-    topic,
-    all,
-    BaseWidget,
-    LoadingIndicator,
-    GraphicsLayer,
-    ArcGISDynamicMapServiceLayer,
-    FeatureLayer,
-    SimpleLineSymbol,
-    FindTask,
-    FindParameters,
-    QueryTask,
-    Query,
-    Color
+], function(
+  declare,
+  lang,
+  array,
+  topic,
+  all,
+  BaseWidget,
+  jimuUtils,
+  LoadingIndicator,
+  GraphicsLayer,
+  ArcGISDynamicMapServiceLayer,
+  FeatureLayer,
+  SimpleLineSymbol,
+  FindTask,
+  FindParameters,
+  QueryTask,
+  Query,
+  Color
 ) {
   var clazz = declare([BaseWidget], {
     name: "FindFeature",
@@ -41,7 +43,10 @@ define([
     defaultPolylineSymbol: null,
     defaultPolygonSymbol: null,
 
-    postCreate: function () {
+    _showResult: true,
+    _centerResult: true,
+
+    postCreate: function() {
       this.inherited(arguments);
       this.resultLayer = new GraphicsLayer();
       this.map.addLayer(this.resultLayer);
@@ -52,16 +57,18 @@ define([
         4
       );
 
-      topic.subscribe("findFeature", lang.hitch(this, this.onTopicHandler_findFeature));
+      topic.subscribe(
+        "findFeature",
+        lang.hitch(this, this.onTopicHandler_findFeature)
+      );
     },
 
-
-    _doFindTask: function (url, ids) {
+    _doFindTask: function(url, ids) {
       var deferredArray = [];
       var loading = new LoadingIndicator();
       loading.placeAt(window.jimuConfig.layoutId);
 
-      array.forEach(ids, function (id) {
+      array.forEach(ids, function(id) {
         var findTask = new FindTask(url);
         var findParam = new FindParameters();
         findParam.searchFields = ["FEATUREID"];
@@ -70,32 +77,43 @@ define([
         findParam.returnGeometry = true;
         deferredArray.push(findTask.execute(findParam));
       });
-      all(deferredArray).then(lang.hitch(this, function (results) {
-        array.forEach(results, function (result) {
-          array.forEach(result, function (findResult) {
-            var graphic = findResult.feature;
-            switch (graphic.geometry.type) {
-              case "polyline":
-                graphic.symbol = this.defaultPolylineSymbol;
-                break;
-            }
+      all(deferredArray).then(
+        lang.hitch(this, function(results) {
+          array.forEach(
+            results,
+            function(result) {
+              array.forEach(
+                result,
+                function(findResult) {
+                  var graphic = findResult.feature;
+                  switch (graphic.geometry.type) {
+                    case "polyline":
+                      graphic.symbol = this.defaultPolylineSymbol;
+                      break;
+                  }
 
-            this.resultLayer.add(graphic);
-          }, this);
-        }, this);
-        loading.destroy();
-      }), function (error) {
-        console.log(error);
-        loading.destroy();
-      });
+                  this.resultLayer.add(graphic);
+                },
+                this
+              );
+            },
+            this
+          );
+          loading.destroy();
+        }),
+        function(error) {
+          console.log(error);
+          loading.destroy();
+        }
+      );
     },
 
-    _doQueryTaskOnFeatureLayer: function (url, ids) {
+    _doQueryTaskOnFeatureLayer: function(url, ids) {
       var loading = new LoadingIndicator();
       loading.placeAt(window.jimuConfig.layoutId);
 
       var where = "";
-      array.forEach(ids, function (id) {
+      array.forEach(ids, function(id) {
         where += "FEATUREID = '" + id + "' OR ";
       });
       //去掉最后的" OR "共四个字符
@@ -106,19 +124,62 @@ define([
       query.returnGeometry = true;
       query.where = where;
       query.outFields = ["*"];
-      queryTask.execute(query, lang.hitch(this, function (featureSet) {
-        console.log(featureSet);
+      queryTask.execute(
+        query,
+        lang.hitch(this, function(featureSet) {
+          featureSet.features.forEach(function(graphic) {
+            if (this._showResult) {
+              var node = graphic.getNode();
+              node.setAttribute("data-highlight", "highlight");
+              setTimeout(function() {
+                node.setAttribute("data-highlight", "");
+              }, 5000);
+            }
+            if (this._centerResult) {
+              this.map.centerAt(jimuUtils.getGeometryCenter(graphic.geometry));
+            }
+          }, this);
 
-        loading.destroy();
-      }), function (error) {
-        console.log(error);
-        loading.destroy();
-      });
+          loading.destroy();
+        }),
+        function(error) {
+          console.log(error);
+          loading.destroy();
+        }
+      );
     },
 
-    onTopicHandler_findFeature: function (params) {
+    _findInGraphicsLayer: function(graphicsLayer, ids) {
+      graphicsLayer.graphics.forEach(function (graphic) {
+        var attr = graphic.attributes;
+        var id;
+        for (var fieldName in attr) {
+          if (attr.hasOwnProperty(fieldName)) {
+            if (fieldName.indexOf("DEVICEID") > -1 || fieldName.indexOf("BM_CODE") > -1 || fieldName.indexOf("FEATUREID") > -1) {
+              id = attr[fieldName];
+            }
+          }
+        }
+        if (ids.indexOf(id) >= 0) {
+          if (this._showResult) {
+            var node = graphic.getNode();
+            node.setAttribute("data-highlight", "highlight");
+            setTimeout(function() {
+              node.setAttribute("data-highlight", "");
+            }, 5000);
+          }
+          if (this._centerResult) {
+            this.map.centerAt(jimuUtils.getGeometryCenter(graphic.geometry));
+          }
+        }
+      }, this);
+    },
+
+    onTopicHandler_findFeature: function(params) {
       var layerName = params.params.layerName || "";
       var ids = params.params.ids || "";
+      this._showResult = params.params.showResult !== false;
+      this._centerResult = params.params.centerResult === true;
 
       if (layerName === "" || ids === "") {
         return;
@@ -128,7 +189,10 @@ define([
       for (var i = 0; i < this.map.layerIds.length; i++) {
         var layer = this.map.getLayer(this.map.layerIds[i]);
         //如果是Dynamic Service, 用FindTask查询服务中的所有图层
-        if (layer.label === layerName && layer instanceof ArcGISDynamicMapServiceLayer) {
+        if (
+          layer.label === layerName &&
+          layer instanceof ArcGISDynamicMapServiceLayer
+        ) {
           this._doFindTask(layer.url, ids);
           return;
         }
@@ -138,7 +202,8 @@ define([
       for (i = 0; i < this.map.graphicsLayerIds.length; i++) {
         layer = this.map.getLayer(this.map.graphicsLayerIds[i]);
         if (layer.label === layerName && layer instanceof FeatureLayer) {
-          this._doQueryTaskOnFeatureLayer(layer.url, ids);
+          // this._doQueryTaskOnFeatureLayer(layer.url, ids);
+          this._findInGraphicsLayer(layer, ids);
         }
       }
     }
