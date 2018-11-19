00dcd8cacfcc16fafc2323e0a039dffd1dc0404d
晓青 史 <herrshi@hotmail.com>
2018-11-19 16:51:37 +0800

    11

--- widgets/BottomToolbar/Widget.js
+++ widgets/BottomToolbar/Widget.js
@@ -85,6 +85,10 @@ define([
 
       var label = domAttr.get(target, "title");
       var enable = domClass.contains(target, this._activeClass);
+      //通知页面
+      if (typeof onBottomButtonClick !== "undefined" && onBottomButtonClick instanceof Function) {
+        onBottomButtonClick(label, enable);
+      }
 
       var operations = domAttr.get(target, "data-operations");
       if (operations !== "undefined") {
