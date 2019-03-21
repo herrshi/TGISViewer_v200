13a6a2400d6a235a564f0611a4d0b4edc7e813a7
herrshi <herrshi1980@gmail.com>
2019-03-21 17:04:29 +0800

    11

--- widgets/JurisdictionPolice/Widget.js
+++ widgets/JurisdictionPolice/Widget.js
@@ -11,10 +11,13 @@ define([
   "dojo/dom-construct",
   "dojo/dom-style",
   "dojo/dom-attr",
+  "dojo/dom-class",
   "dojo/_base/window",
   "jimu/BaseWidget",
   "esri/graphic",
   "esri/layers/FeatureLayer",
+  "esri/layers/LabelClass",
+  "esri/symbols/TextSymbol",
   "esri/renderers/SimpleRenderer"
 ], function(
   declare,
@@ -24,16 +27,20 @@ define([
   domConstruct,
   domStyle,
   domAttr,
+  domClass,
   win,
   BaseWidget,
   Graphic,
   FeatureLayer,
+  LabelClass,
+  TextSymbol,
   SimpleRenderer
 ) {
   return declare([BaseWidget], {
     labelPointGraphics: [],
-    labelPointDivs: [],
+    policeCountDivs: [],
     jurisdictionLayer: null,
+    jurisdictionLabelLayer: null,
 
     postCreate: function() {
       this.inherited(arguments);
@@ -42,10 +49,22 @@ define([
       this.map.on("zoom-end", lang.hitch(this, this._placeDiv));
       this.map.on("pan-end", lang.hitch(this, this._placeDiv));
 
-      topic.subscribe("showJurisdiction", lang.hitch(this, this.onTopicHandler_showJurisdiction));
-      topic.subscribe("hideJurisdiction", lang.hitch(this, this.onTopicHandler_hideJurisdiction));
-      topic.subscribe("showPoliceCount", lang.hitch(this, this.onTopicHandler_showPoliceCount))
-      topic.subscribe("hidePoliceCount", lang.hitch(this, this.onTopicHandler_hidePoliceCount))
+      topic.subscribe(
+        "showJurisdiction",
+        lang.hitch(this, this.onTopicHandler_showJurisdiction)
+      );
+      topic.subscribe(
+        "hideJurisdiction",
+        lang.hitch(this, this.onTopicHandler_hideJurisdiction)
+      );
+      topic.subscribe(
+        "showPoliceCount",
+        lang.hitch(this, this.onTopicHandler_showPoliceCount)
+      );
+      topic.subscribe(
+        "hidePoliceCount",
+        lang.hitch(this, this.onTopicHandler_hidePoliceCount)
+      );
     },
 
     _readLayer: function() {
@@ -62,16 +81,32 @@ define([
 
                 this.jurisdictionLayer = new FeatureLayer(featureCollection, {
                   outFields: ["*"],
-                  visible: false
+                  visible: false,
+                  showLabels: true
                 });
+                this.jurisdictionLayer.setLabelingInfo([
+                  {
+                    labelExpressionInfo: { value: "{SHOWNAME}" },
+                    symbol: {
+                      type: "esriTS",
+                      color: [36, 135, 205],
+                      font: {
+                        family: "Microsoft YaHei",
+                        size: 10,
+                        weight: "bold"
+                      }
+                    }
+                  }
+                ]);
                 var renderer = new SimpleRenderer({
                   type: "simple",
                   symbol: {
                     type: "esriSFS",
-                    style: "esriSFSNull",
+                    style: "esriSFSSolid",
+                    color: [0, 45, 80, 178],
                     outline: {
-                      color: [0, 174, 239, 255],
-                      width: 2,
+                      color: [0, 210, 245, 230],
+                      width: 1,
                       type: "esriSLS",
                       style: "esriSLSSolid"
                     }
@@ -85,7 +120,7 @@ define([
         })
       );
 
-      //辖区标注
+      //警力统计
       fetch(
         window.path + "configs/JurisdictionPolice/Jurisdiction_Point.json"
       ).then(
@@ -106,15 +141,21 @@ define([
     },
 
     _createDiv: function() {
-      this.labelPointDivs = this.labelPointGraphics.map(function(graphic) {
+      this.policeCountDivs = this.labelPointGraphics.map(function(graphic) {
         var id = graphic.attributes["FEATUREID"];
         var name = graphic.attributes["SHOWNAME"];
         return domConstruct.place(
-          "<div id='" + ("Jurisdiction" + id) + "' " +
+          "<div id='" +
+            ("Jurisdiction" + id) +
+            "' " +
             "class='jingli_point_green' " +
             "style='position:absolute; z-index: 99; display: none'>" +
-              "<span id='" + ("JurisdictionCount" + id) + "'>100</span>" +
-              "<span class='jingli_point_font'>" + name + "</span>" +
+            "<span id='" +
+            ("JurisdictionCount" + id) +
+            "'>100</span>" +
+            "<span class='jingli_point_font'>" +
+            name +
+            "</span>" +
             "</div>",
           win.body()
         );
@@ -126,34 +167,49 @@ define([
         var screenPoint = this.map.toScreen(graphic.geometry);
         var id = graphic.attributes["FEATUREID"];
         var div = dom.byId("Jurisdiction" + id);
-        domStyle.set(div, {left: screenPoint.x - 20 + "px", top: screenPoint.y - 100 + "px"});
+        domStyle.set(div, {
+          left: screenPoint.x - 20 + "px",
+          top: screenPoint.y - 70 + "px"
+        });
       }, this);
     },
 
-    onTopicHandler_showJurisdiction: function () {
+    onTopicHandler_showJurisdiction: function() {
       this.jurisdictionLayer.setVisibility(true);
     },
 
-    onTopicHandler_hideJurisdiction: function () {
+    onTopicHandler_hideJurisdiction: function() {
       this.jurisdictionLayer.setVisibility(false);
     },
 
-    onTopicHandler_showPoliceCount: function (params) {
-      this.labelPointDivs.forEach(function (div) {
-        domStyle.set(div, {display: "block"});
+    onTopicHandler_showPoliceCount: function(params) {
+      this.jurisdictionLayer.showLabels = false;
+      this.jurisdictionLayer.refresh();
+
+      this.policeCountDivs.forEach(function(div) {
+        domStyle.set(div, { display: "block" });
       });
 
       if (params && params instanceof Array) {
-        params.forEach(function (countData) {
-          var node = dom.byId("JurisdictionCount" + countData.id);
-          node.innerHTML = countData.count;
+        params.forEach(function(countData) {
+          var parentNode = dom.byId("Jurisdiction" + countData.id);
+          domClass.remove(
+            parentNode,
+            "jingli_point_green jingli_point_orange jingli_point_red"
+          );
+          domClass.add(parentNode, countData.class || "jingli_point_green");
+          var countNode = dom.byId("JurisdictionCount" + countData.id);
+          countNode.innerHTML = countData.count;
         }, this);
       }
     },
 
-    onTopicHandler_hidePoliceCount: function () {
-      this.labelPointDivs.forEach(function (div) {
-        domStyle.set(div, {display: "none"});
+    onTopicHandler_hidePoliceCount: function() {
+      this.jurisdictionLayer.showLabels = true;
+      this.jurisdictionLayer.refresh();
+
+      this.policeCountDivs.forEach(function(div) {
+        domStyle.set(div, { display: "none" });
       });
     }
   });
