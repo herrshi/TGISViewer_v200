0b6b448aa1bc63a76a447d1344d4c4513f9053dd
herrshi <herrshi1980@gmail.com>
2019-05-08 17:47:32 +0800

    11

--- widgets/Route/Widget.js
+++ widgets/Route/Widget.js
@@ -19,7 +19,7 @@ define([
     },
 
     /**获取地图点击坐标*/
-    _getMapClickedCoordinate: function(type) {
+    _getMapClickedCoordinate: function() {
       var def = new Deferred();
       this.map.on("click", function(event) {
         var mapPoint = event.mapPoint;
@@ -34,18 +34,52 @@ define([
     /**逆地理编码，获取地图点击位置的地名*/
     _reGeoCode: function(x, y) {
       var def = new Deferred();
-
+      var regeoUrl = this.config.regeoUrl.replace(
+        /{gisServer}/i,
+        this.appConfig.gisServer
+      );
+      $.get(
+        regeoUrl,
+        {
+          ak: this.config.ak,
+          location: x + "," + y,
+          pois: 1
+        },
+        function(response, status) {
+          if (status === "success") {
+            def.resolve(response);
+          } else {
+            def.reject();
+          }
+        },
+        "jsonp"
+      );
       return def;
     },
 
     onInputRouteStart_focus: function() {
-      this._getMapClickedCoordinate("start").then(lang.hitch(this, function (startPoint) {
-        this._reGeoCode(startPoint.x, startPoint.y);
-      }));
+      this._getMapClickedCoordinate().then(
+        lang.hitch(this, function(startPoint) {
+          this._reGeoCode(startPoint.x, startPoint.y).then(function (response) {
+            if (response.message === "ok") {
+              var result = response.result[0];
+              var poiName;
+              if (result.pois.length === 0) {
+                poiName = result.formatted_address;
+              } else {
+                poiName = result.pois[0].name;
+              }
+              $("#inputRouteStart").val(poiName);
+
+              $("#inputRouteEnd").trigger("focus");
+            }
+          });
+        })
+      );
     },
 
     onInputRouteEnd_focus: function() {
-      this._getMapClickedCoordinate("end");
+      this._getMapClickedCoordinate();
     },
 
     onBtnClose_click: function(event) {
