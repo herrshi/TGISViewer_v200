8e3c2ebdfe1cdaf6ae6c630824947bc40998dbe1
herrshi <herrshi1980@gmail.com>
2019-05-09 14:13:32 +0800

    11

--- widgets/Route/Widget.js
+++ widgets/Route/Widget.js
@@ -4,13 +4,50 @@ define([
   "dojo/topic",
   "dojo/Deferred",
   "jimu/BaseWidget",
+  "esri/graphic",
+  "esri/geometry/Point",
+  "esri/layers/GraphicsLayer",
+  "esri/symbols/PictureMarkerSymbol",
   "esri/geometry/webMercatorUtils"
-], function(declare, lang, topic, Deferred, BaseWidget, webMercatorUtils) {
+], function(
+  declare,
+  lang,
+  topic,
+  Deferred,
+  BaseWidget,
+  Graphic,
+  Point,
+  GraphicsLayer,
+  PictureMarkerSymbol,
+  webMercatorUtils
+) {
   return declare([BaseWidget], {
     baseClass: "jimu-widget-Route",
 
+    startPointSymbol: null,
+    endPointSymbol: null,
+    startPoint: null,
+    endPoint: null,
+    routeLayer: null,
+
     postCreate: function() {
       this.inherited(arguments);
+
+      this.startPointSymbol = new PictureMarkerSymbol({
+        url: window.path + "images/mapIcons/icon_gis_green_baojing.png",
+        width: 23.625,
+        height: 30.75,
+        yoffset: 15.375
+      });
+      this.endPointSymbol = new PictureMarkerSymbol({
+        url: window.path + "images/mapIcons/icon_gis_red_baojing.png",
+        width: 23.625,
+        height: 30.75,
+        yoffset: 15.375
+      });
+
+      this.routeLayer = new GraphicsLayer();
+      this.map.addLayer(this.routeLayer);
 
       topic.subscribe(
         "showRoute",
@@ -46,8 +83,14 @@ define([
           pois: 1
         },
         function(response, status) {
-          if (status === "success") {
-            def.resolve(response);
+          if (status === "success" && response.message === "ok") {
+            //附近没有poi就用位置描述，有poi就用最近的poi名称
+            var result = response.result[0];
+            var poiName =
+              result.pois.length === 0
+                ? result.formatted_address
+                : result.pois[0].name;
+            def.resolve(poiName);
           } else {
             def.reject();
           }
@@ -57,32 +100,85 @@ define([
       return def;
     },
 
+    _findRoute: function() {
+      var routeUrl = this.config.routeUrl.replace(
+        /{gisServer}/i,
+        this.appConfig.gisServer
+      );
+    },
+
+    /**起点输入框focus后，在地图上点击获取起点*/
     onInputRouteStart_focus: function() {
       this._getMapClickedCoordinate().then(
-        lang.hitch(this, function(startPoint) {
-          this._reGeoCode(startPoint.x, startPoint.y).then(function (response) {
-            if (response.message === "ok") {
-              var result = response.result[0];
-              var poiName;
-              if (result.pois.length === 0) {
-                poiName = result.formatted_address;
-              } else {
-                poiName = result.pois[0].name;
-              }
-              $("#inputRouteStart").val(poiName);
+        lang.hitch(this, function(point) {
+          //添加起点图标
+          var graphic = new Graphic(point, this.startPointSymbol);
+          graphic.id = "start";
+          this.routeLayer.add(graphic);
 
-              $("#inputRouteEnd").trigger("focus");
-            }
-          });
+          this._reGeoCode(point.x, point.y).then(lang.hitch(this, function(poiName) {
+            this.startPoint = point;
+            $("#inputRouteStart").val(poiName);
+            //确定起点以后自动开始输入终点
+            $("#inputRouteEnd").trigger("focus");
+          }));
         })
       );
     },
 
-    onInputRouteEnd_focus: function() {
-      this._getMapClickedCoordinate();
+    onBtnClearStart_click: function() {
+      this.startPoint = null;
+
+      for (var i = 0; i < this.routeLayer.graphics.length; i++) {
+        var graphic = this.routeLayer.graphics[i];
+        if (graphic.id === "start") {
+          this.routeLayer.remove(graphic);
+        }
+      }
+
+      var inputRouteStart = $("#inputRouteStart");
+      inputRouteStart.val("");
+      inputRouteStart.trigger("focus");
     },
 
-    onBtnClose_click: function(event) {
+    onInputRouteEnd_focus: function() {
+      this._getMapClickedCoordinate().then(
+        lang.hitch(this, function(point) {
+
+          //添加终点图标
+          var graphic = new Graphic(point, this.endPointSymbol);
+          graphic.id = "end";
+          this.routeLayer.add(graphic);
+
+          this._reGeoCode(point.x, point.y).then(lang.hitch(this, function(poiName) {
+            this.endPoint = point;
+            $("#inputRouteEnd").val(poiName);
+
+            if (this.startPoint && this.endPoint) {
+              this._findRoute();
+            }
+          }));
+        })
+      );
+    },
+
+    onBtnClearEnd_click: function() {
+      this.endPoint = null;
+
+      for (var i = 0; i < this.routeLayer.graphics.length; i++) {
+        var graphic = this.routeLayer.graphics[i];
+        if (graphic.id === "end") {
+          this.routeLayer.remove(graphic);
+        }
+      }
+
+      var inputRouteEnd = $("#inputRouteEnd");
+      inputRouteEnd.val("");
+      inputRouteEnd.trigger("focus");
+
+    },
+
+    onBtnClose_click: function() {
       $("." + this.baseClass).addClass("hide");
     },
 
@@ -95,6 +191,9 @@ define([
       baseDom.draggable();
       baseDom.removeClass("hide");
       $("#routeParam,#routeResult").removeClass("hide");
+
+      //输入起点
+      $("#inputRouteStart").trigger("focus");
     }
   });
 });
